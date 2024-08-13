0023167b303ed898a42b78f807bc132878a24aae
Sanket Poojary <164500111+sanket-poojary-03@users.noreply.github.com>
2024-08-13 09:56:08 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,7 +1,12 @@
 import torch
-
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
+import evaluate
+from transformers import Seq2SeqTrainingArguments
+from transformers import Seq2SeqTrainer
+
+from huggingface_hub import notebook_login
+notebook_login()
 
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
@@ -10,54 +15,34 @@ class DataCollatorSpeechSeq2SeqWithPadding:
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         
         input_features = [{"input_features": feature["input_features"]} for feature in features]
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
-
-        
-        label_features = [{"input_ids": feature["labels"]} for feature in features]
-        
+        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")   
+        label_features = [{"input_ids": feature["labels"]} for feature in features]    
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
-
-        
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
-        
+        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)     
         if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
             labels = labels[:, 1:]
-
         batch["labels"] = labels
-
         return batch
     
 data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
 
-import evaluate
 
 metric = evaluate.load("wer")
 def compute_metrics(pred):
     pred_ids = pred.predictions
-    label_ids = pred.label_ids
-
-    
-    label_ids[label_ids == -100] = tokenizer.pad_token_id
-
-    
+    label_ids = pred.label_ids    
+    label_ids[label_ids == -100] = tokenizer.pad_token_id    
     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-
     wer = 100 * metric.compute(predictions=pred_str, references=label_str)
-
     return {"wer": wer}
+
 
 model.config.forced_decoder_ids = None
 model.config.suppress_tokens = []
 model.generation_config.language = "en"
 
-from huggingface_hub import notebook_login
 
-notebook_login()
-
-
-from transformers import Seq2SeqTrainingArguments
 
 training_args = Seq2SeqTrainingArguments(
     output_dir="./whisper-darpg",
@@ -83,7 +68,6 @@ training_args = Seq2SeqTrainingArguments(
 )
 
 
-from transformers import Seq2SeqTrainer
 
 trainer = Seq2SeqTrainer(
     args=training_args,
